1743b6027f2b473a7ab97208db45180808a72558
Carmen Collins <hellocarmencollins@gmail.com>
2018-08-29 14:29:22 -0500

    updating survey questions and hardcoding in a few more friends

--- server.js
+++ server.js
@@ -40,7 +40,17 @@ var friends = [
     {
         "name" : "Carmen",
         "photo" : "https://placehold.it/200x200",
-        "scores" : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        "scores" : [2, 5, 3, 4, 4, 3, 2, 1, 3, 5]
+    },
+    {
+        "name" : "Olivia",
+        "photo" : "https://placehold.it/200x200",
+        "scores" : [3, 2, 4, 4, 4, 1, 2, 4, 1, 3]
+    },
+    {
+        "name" : "Natalie",
+        "photo" : "https://placehold.it/200x200",
+        "scores" : [5, 2, 2, 2, 5, 2, 5, 3, 2, 5]
     }
 ];
 
